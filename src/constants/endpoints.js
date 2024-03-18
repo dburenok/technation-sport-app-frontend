33845d34e3edb4622c24a8d7bb2841245b3067a4
Dmitriy Burenok <dmitriyburenok@gmail.com>
2024-03-18 11:16:45 -0700

    add backend URL env variable

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,7 @@
-export const ATHLETE_QUESTIONS_URL = "http://localhost:3000/questions";
-export const COACH_QUESTIONS_URL = "http://localhost:3000/coach-questions";
-export const SAVE_USER_URL = "http://localhost:3000/save-user";
-export const IMAGE_PATH_URL = "http://localhost:3000/get-image-path";
-export const MEAL_PLAN_URL = "http://localhost:3000/meal-plan";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+export const ATHLETE_QUESTIONS_URL = `${BACKEND_URL}/questions`;
+export const COACH_QUESTIONS_URL = `${BACKEND_URL}/coach-questions`;
+export const SAVE_USER_URL = `${BACKEND_URL}/save-user`;
+export const IMAGE_PATH_URL = `${BACKEND_URL}/get-image-path`;
+export const MEAL_PLAN_URL = `${BACKEND_URL}/meal-plan`;
